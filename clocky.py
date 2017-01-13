d0fc473e4adadc023050af7f48fd741cbfb697ba
Mauro <lvm@users.noreply.github.com>
2017-01-13 16:02:59 -0300

    Create clocky.py

---
+++ clocky.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import time
+
+def clocky(f):
+    def tick(*args, **kwargs):
+        _start = time.time()
+        _func = f(*args, **kwargs)
+        _end = time.time()
+
+        t_sec = round(_end-_start)
+        (t_min, t_sec) = divmod(t_sec,60)
+        (t_hour,t_min) = divmod(t_min,60)
+        print ("Time passed: {}:{}:{}".format(t_hour,t_min,t_sec))
+
+        return _func
+    return tick
